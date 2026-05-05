a9f408165fc687f614adc5ddbfce02a59ae263ca
voyageedtravel-hash <voyageedtravel@gmail.com>
2026-05-05 16:55:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(cors({
   origin: [
     'https://capable-naiad-cfa4af.netlify.app',
     'https://splendid-cactus-c8b150.netlify.app',
-    'https://resonant-blancmange-4d3a03.netlify.app',  // ← NEW: Added this line
+    'https://resonant-blancmange-4d3a03.netlify.app',
+    'https://animated-donut-eb7622.netlify.app',  // ← NEW: Added this line
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:5000'
